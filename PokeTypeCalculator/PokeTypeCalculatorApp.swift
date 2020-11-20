49d507a42b0f8b09437f356487d5db5fc235e7c6
Brandon Jenniges <steveholt55@gmail.com>
2020-11-19 21:36:49 -0600

    Initial Commit

---
+++ PokeTypeCalculator/PokeTypeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeTypeCalculatorApp.swift
+//  PokeTypeCalculator
+//
+//  Created by Brandon Jenniges on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct PokeTypeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
