d9940e526185cddd880063c8205d4a8dee972b51
Brandon Jenniges <brandon.jenniges@icloud.com>
2021-05-25 20:56:50 -0500

    Fix rotation

--- PokeTypeCalculator/Extension/UIViewController+Child.swift
+++ PokeTypeCalculator/Extension/UIViewController+Child.swift
@@ -10,8 +10,17 @@ import UIKit
 extension UIViewController {
     func add(_ child: UIViewController, parentView: UIView? = nil) {
         addChild(child)
-        child.view.frame = (parentView ?? view).frame
         (parentView ?? view).addSubview(child.view)
+        guard let superview = child.view.superview else {
+            fatalError()
+        }
+        child.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            child.view.topAnchor.constraint(equalTo: superview.topAnchor),
+            child.view.bottomAnchor.constraint(equalTo: superview.bottomAnchor),
+            child.view.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            child.view.trailingAnchor.constraint(equalTo: superview.trailingAnchor)
+        ])
         child.didMove(toParent: self)
     }
 
